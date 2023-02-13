297920d3d17881b98fa3be9e7c213d73d9e68b96
June <khazoda@gmail.com>
2023-02-13 15:33:19 +0000

    + test page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="w-full h-screen flex justify-center items-center">
+    <div class="chat chat-start">
+        <div class="chat-image avatar">
+          <div class="w-10 rounded-full">
+            <img src="https://yt3.ggpht.com/fL5t5DZL3yXCA3UIf88Ec6PwOUw749HXNdw2iUKNce-y2sg365bjlTLkSWbisX5B_Vmd4zufDoA=s88-c-k-c0x00ffffff-no-rj-mo" />
+          </div>
+        </div>
+        <div class="chat-bubble">It was said that you would, destroy the Sith, not join them.</div>
+      </div>
+</main>
