dfd5d98f47e6c3159afee5e058008ba29778fa53
June <khazoda@gmail.com>
2023-02-14 00:57:44 +0000

    rough page finality

---
+++ src/lib/Card.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let siteName = 'Default';
+	export let siteDescription = 'Default description of site very longvery longvery long ';
+	export let siteWingding = '/personal_site.png';
+</script>
+
+<!-- 
+<div class="card max-w-[450px] max-h-[150px] m-6 bg-base-100 shadow-xl image-full  ">
+	<figure><img src={siteBackground} alt="Shoes" /></figure>
+	<div class="card-body">
+		<h2 class="card-title">{siteName}</h2>
+		<p>{siteDescription}</p>
+		<div class="card-actions justify-end">
+			<button class="btn btn-primary">Visit Website</button>
+		</div>
+	</div>
+</div> -->
+
+<div id="card-wrapper" class="card-grid shadow-xl">
+	<h1 id="card-title" class="text-2xl p-4">{siteName}</h1>
+	<img id="card-wingding" src={siteWingding} alt={siteWingding} class="w-20 ml-auto p-4" />
+	<div id="card-description" class="row-span-2 p-4">
+		{siteDescription}
+	</div>
+	<div
+		id="card-button-cutout"
+		class="absolute bottom-0 right-0 w-[156px] h-[55px] ml-auto  bg-base-100"
+	/>
+	<button
+		id="card-button"
+		class="btn btn-outline btn-success shadow col-start-2 col-end-2 row-start-3 row-end-3 z-10 rounded-none rounded-br-2xl self-end"
+		>Visit Website</button
+	>
+</div>
+
+<style>
+	.card-grid {
+		position: relative;
+		margin: 0.5rem;
+		max-width: 450px;
+		max-height: 180px;
+
+		display: grid;
+		grid-template-columns: 2fr 1fr;
+		grid-template-rows: 3rem 1fr 1fr;
+		gap: 0.25rem;
+
+		background: #191d24;
+		border-radius: 1rem;
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,73 @@
-<main class="w-full h-screen flex justify-center items-center">
-    <div class="chat chat-start">
-        <div class="chat-image avatar">
-          <div class="w-10 rounded-full">
-            <img src="https://yt3.ggpht.com/fL5t5DZL3yXCA3UIf88Ec6PwOUw749HXNdw2iUKNce-y2sg365bjlTLkSWbisX5B_Vmd4zufDoA=s88-c-k-c0x00ffffff-no-rj-mo" />
-          </div>
-        </div>
-        <div class="chat-bubble">It was said that you would, destroy the Sith, not join them.</div>
-      </div>
+<script>
+	import Card from '$lib/Card.svelte';
+	import { onMount } from 'svelte';
+
+	let visible = false;
+	onMount(() => (visible = true));
+	/**
+	 * @param {HTMLDivElement} node
+	 */
+	function typewriter(node, { speed = 1 }) {
+		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`);
+		}
+
+		const text = node.textContent || 'Error';
+		const duration = text.length / (speed * 0.01);
+
+		return {
+			duration,
+			tick: (/** @type {number} */ t) => {
+				const i = Math.trunc(text.length * t);
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
+</script>
+
+<main class="m-auto max-w-[680px] w-screen h-screen flex flex-col items-center justify-center">
+	<div id="speech-wrapper" class="chat chat-start mt-6">
+		<div id="speech-avatar" class="chat-image avatar">
+			<div class="w-10 rounded-full">
+				<img
+					alt="June's avatar"
+					src="https://yt3.ggpht.com/fL5t5DZL3yXCA3UIf88Ec6PwOUw749HXNdw2iUKNce-y2sg365bjlTLkSWbisX5B_Vmd4zufDoA=s88-c-k-c0x00ffffff-no-rj-mo"
+				/>
+			</div>
+		</div>
+		{#if visible}
+			<div
+				transition:typewriter={{ speed: 4 }}
+				id="speech-text"
+				class="chat-bubble min-h-[32px] max-w-[680px] overflow-ellipsis overflow-hidden"
+			>
+				Thanks for visiting, please check out the links below.
+			</div>
+		{/if}
+	</div>
+
+	<div class="flex flex-col justify-center gap-1">
+		<Card
+			siteName="Personal Site"
+			siteWingding="/personal_site.svg"
+			siteDescription="Browse my previous work and personal projects"
+		/>
+		<Card
+			siteName="SeaCroak"
+			siteWingding="/seacroak.svg"
+			siteDescription="Explore the applications, games and projects I've released under the SeaCroak brand"
+		/>
+		<Card
+			siteName="Modrinth"
+			siteWingding="/modrinth.svg"
+			siteDescription="Repository of my mods on Modrinth"
+		/>
+		<Card
+			siteName="CurseForge"
+			siteWingding="/curseforge.svg"
+			siteDescription="Repository of my mods on CurseForge"
+		/>
+	</div>
 </main>
