ab0bf20bedb9e4ac9f056fd79fe0dd2cbc6a1c9a
June <khazoda@gmail.com>
2023-02-14 02:57:14 +0000

    new button animation

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { slide } from 'svelte/transition';
+
 	export let siteName = 'Default';
 	export let siteDescription = 'Default description of site very longvery longvery long ';
 	export let siteWingding = '/personal_site.png';
@@ -16,19 +18,24 @@
 	</div>
 </div> -->
 
-<div id="card-wrapper" class="card-grid shadow-xl">
-	<h1 id="card-title" class="text-2xl p-4">{siteName}</h1>
-	<img id="card-wingding" src={siteWingding} alt={siteWingding} class="w-20 ml-auto p-4" />
-	<div id="card-description" class="row-span-2 p-4">
+<div
+	id="card-wrapper"
+	class="card-grid drop-shadow-lg bg-[url('/card-bg.svg')] bg-no-repeat bg-cover outline-success rounded-br-xl"
+>
+	<h1 id="card-title" class="text-2xl pl-4 pt-2">{siteName}</h1>
+	<img
+		id="card-wingding"
+		src={siteWingding}
+		alt={siteWingding}
+		class="w-20 ml-auto pt-4 pr-4 pl-2 pb-2"
+	/>
+	<div id="card-description" class="row-span-2 p-4 pb-2 text-gray-300">
 		{siteDescription}
 	</div>
-	<div
-		id="card-button-cutout"
-		class="absolute bottom-0 right-0 w-[156px] h-[55px] ml-auto  bg-base-100"
-	/>
+
 	<button
 		id="card-button"
-		class="btn btn-outline btn-success shadow col-start-2 col-end-2 row-start-3 row-end-3 z-10 rounded-none rounded-br-2xl self-end"
+		class="relative btn btn-outline btn-success shadow col-start-2 col-end-2 row-start-3 row-end-3 z-10 rounded-none rounded-br-none self-end hover:rounded-br-3xl transition-all"
 		>Visit Website</button
 	>
 </div>
@@ -45,9 +52,7 @@
 		grid-template-rows: 3rem 1fr 1fr;
 		gap: 0.25rem;
 
-		background: #191d24;
+		/* background: #191d24; */
 		border-radius: 1rem;
-		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
-			var(--tw-shadow);
 	}
 </style>
---
+++ src/lib/SpeechSequence.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const activeSentence = 'Thanks for visiting, please check out the links below.';
+	let visible = false;
+	onMount(() => {
+		visible = true;
+	});
+
+	function typewriter(node: HTMLDivElement, { speed = 1 }: { speed: number }) {
+		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`);
+		}
+
+		const text = node.textContent || 'Error';
+		const duration = text.length / (speed * 0.01);
+		console.log(duration);
+		return {
+			duration,
+			tick: (t: number) => {
+				const i = Math.trunc(text.length * t);
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
+</script>
+
+<div id="speech-wrapper" class="chat chat-start mt-6 mb-2">
+	<div id="speech-avatar" class="chat-image avatar">
+		<div class="w-10 rounded-full shadow-xl">
+			<img
+				alt="June's avatar"
+				src="https://yt3.ggpht.com/fL5t5DZL3yXCA3UIf88Ec6PwOUw749HXNdw2iUKNce-y2sg365bjlTLkSWbisX5B_Vmd4zufDoA=s88-c-k-c0x00ffffff-no-rj-mo"
+			/>
+		</div>
+	</div>
+	{#if visible}
+		<div
+			transition:typewriter={{ speed: 4 }}
+			id="speech-text"
+			class="chat-bubble max-w-[680px] overflow-ellipsis overflow-hidden text-md max-h-[2.5rem] min-h-[2.5rem]"
+		>
+			{activeSentence}
+		</div>
+	{/if}
+</div>
